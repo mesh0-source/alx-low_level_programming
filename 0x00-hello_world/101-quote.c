20689f19526eb56b2682ce1bafd850a094c6399c
Meshack Barngetuny <mngetich97@gmail.com>
2022-09-09 12:07:09 -0700

    Script 1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - prints a text
+ *Return: Always 1
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Kopara, 2015-10-19\n", 59);
+	return(1);
+}
+
