0469eee3dc2554c7ef76f0b15e4155c6e432c5a1
Meshack Barngetuny <mngetich97@gmail.com>
2022-09-10 23:38:58 -0700

    Script 1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,11 +9,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if n>0;
+	if(n>0);
 		printf(%dn "is positive")
-	else if n=0;
+	else if(n=0);
 		printf(%dn "is zero")
-	else if n<0;
+	else if(n<0);
 		printf(%dn "is negative" )	
 	return (0);
 }
