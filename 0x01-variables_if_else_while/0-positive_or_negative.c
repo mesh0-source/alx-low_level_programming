39d82c8f764cb97fa6eddecaba987a4797352943
Meshack Barngetuny <mngetich97@gmail.com>
2022-09-11 00:06:15 -0700

    Script 1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/*more headers go here */
 
+/*more headers go here */
 /*main - Assigning a random number n */
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
