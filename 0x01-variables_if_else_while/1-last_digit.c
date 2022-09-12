f0bac636f1ed771dc91826e82c1ef394554cd8e3
Meshack Barngetuny <mngetich97@gmail.com>
2022-09-12 14:36:50 -0700

    Script 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,24 +11,15 @@
 int main(void)
 /*  prints the sign magnitude of n */
 {
-	int n;
-	int m;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	m = (n % 10);
-	if (m > 5)
-	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, m);
-	}
-	else if (m == 0)
-	{
-		printf("Last digit of %d is %d and is 0\n", n, m);
-	}
-	else if (m < 6 && m != 0)
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
-	}
+	char c;
+	clrscr ();
+	for (c = 'a'; c = 'z'; c++)
+		putchar(c);
+	putchar("\n");
+	for (c = 'A'; c = 'Z'; c++)
+		putchar(c);
+	putchar("\n");
+	getch();
 	return (0);
 }
 
