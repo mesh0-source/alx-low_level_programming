a16895347540e8a89ae019c9a2657ba1d59e72bd
Meshack Barngetuny <mngetich97@gmail.com>
2022-09-14 09:49:52 -0700

    Script 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,8 +7,15 @@
  * 	   On error, -1 is returned, and errno is set appropriately.
  *
  */
-int _putchar(char c);
+int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ++ch)
+		_putchar(str[ch]);
+	_putchar('\n');
+
+	return (0);
 }
 
