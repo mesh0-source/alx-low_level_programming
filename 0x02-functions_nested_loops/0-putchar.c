5904e4350108f70ee54c3c617a6244332a9e639a
Meshack Barngetuny <mngetich97@gmail.com>
2022-09-14 09:40:40 -0700

    Script 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,14 @@
-#include "main.h"
+#include <unistd.h>
 /**
- *main - Assigning a number n
- *Returm:0
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * 	   On error, -1 is returned, and errno is set appropriately.
+ *
  */
-
-int main(void)
+int _putchar(char c);
 {
-	putchar('_putchar');
-	putchar('\n');
-	return (0);
+	return (write(1, &c, 1));
 }
 
